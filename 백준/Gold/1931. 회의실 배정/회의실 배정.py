3e6c15eae53b3a07e81a3774a2ee647966c1bb25
hjy080530 <164326669+hjy080530@users.noreply.github.com>
2025-03-06 13:59:09 +0900

    [Gold V] Title: 회의실 배정, Time: 240 ms, Memory: 54216 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,17 @@
+import sys
+
+
+n = int(sys.stdin.readline().strip())
+meetings = [tuple(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+meetings.sort(key=lambda x: (x[1], x[0]))
+
+count = 0
+end_time = 0
+
+for start, end in meetings:
+    if start >= end_time:
+        count += 1
+        end_time = end
+
+print(count)
