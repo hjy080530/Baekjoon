5e2cbae9351170301770e25fdfa02fea063bb539
hjy080530 <164326669+hjy080530@users.noreply.github.com>
2025-03-28 00:18:43 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 56 ms, Memory: 34900 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+n, k = map(int, input().split())
+q = deque(range(1, n + 1)) 
+res = []
+
+while q:
+    q.rotate(-(k-1))
+    res.append(q.popleft())
+
+print("<" + ", ".join(map(str, res)) + ">")
