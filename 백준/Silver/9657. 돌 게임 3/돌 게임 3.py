91ecb0c74f694183349d8aa550a027735d6dab44
hjy080530 <164326669+hjy080530@users.noreply.github.com>
2025-08-08 00:50:35 +0900

    [Silver III] Title: 돌 게임 3, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
@@ -0,0 +1,13 @@
+n = int(input())
+dp = [False] * (n + 5)
+
+dp[1] = True
+dp[2] = False
+dp[3] = True
+dp[4] = True
+
+for i in range(5, n + 1):
+    if not (dp[i-1] and dp[i-3] and dp[i-4]):
+        dp[i] = True
+
+print("SK" if dp[n] else "CY")
